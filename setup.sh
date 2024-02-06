99a381cb276d245f25be504eac3dc506afbae41d
Craig Jackson <igetcheapthrills@gmail.com>
2024-02-07 09:06:34 +1100

    Add xdg-desktop-portal-hyprland and cleanup script

--- setup.sh
+++ setup.sh
@@ -1,100 +1,92 @@
 #!/usr/bin/env bash
 
-export PATH=$PATH:~/.local/bin:~/.cargo/bin:~/.venv/bin
-which git || sudo apt install git
-which curl || sudo apt install curl
-which slurp || sudo apt install slurp
-which grim || sudo apt install grim
-which wl-clipboard || sudo apt install wl-clipboard
-which lxappearance || sudo apt install lxappearance
-which pactl || sudo apt install pulseaudio-utils
-
-export PATH=$HOME/.local/bin:$HOME/.venv/bin:$PATH
 [ -d ~/.venv ] || python3 -m venv ~/.venv
 
+export PATH=$PATH:~/.local/bin:~/.cargo/bin:~/.venv/bin
+
+sudo apt install qttools5-dev-tools libsdbus-c++-dev git curl slurp grim wl_clipboard lxappearance pulseaudio-utils \
+	clang-tidy gobject-introspection libdbusmenu-gtk3-dev libevdev-dev libfmt-dev libgirepository1.0-dev libgtk-3-dev \
+	libgtkmm-3.0-dev libinput-dev libjsoncpp-dev libmpdclient-dev libnl-3-dev libnl-genl-3-dev libpulse-dev libsigc++-2.0-dev \
+	libspdlog-dev libwayland-dev scdoc upower libxkbregistry-dev
+
 # RUST
-curl https://sh.rustup.rs -sSf| sh
+which rustup || curl https://sh.rustup.rs -sSf | sh
+
 rustup default stable
 
 mkdir -p ~/Work
 
 # CATCH2
-cd ~/Work
-git clone https://github.com/catchorg/Catch2 --recurse-submodules
-cd Catch2
+git clone https://github.com/catchorg/Catch2 --recurse-submodules ~/Work/Catch2
+cd ~/Work/Catch22 || exit 2
 meson setup --reconfigure --prefix=/usr/local build
 ninja -C build
 ninja -C build install
 
-
 # SWAYLOCK WITH EFFECTS
-cd ~/Work
-git clone https://github.com/jirutka/swaylock-effects --recurse-submodules
-cd swaylock-effects
+git clone https://github.com/jirutka/swaylock-effects --recurse-submodules ~/Work/swaylock-effects/
+cd ~/Work/swaylock-effects || exit 2
 meson setup --reconfigure --prefix=/usr/local build
 ninja -C build
 ninja -C build install
 
 # SWWW
-cd ~/Work
-git clone https://github.com/LGFae/swww
-cd swww
+git clone https://github.com/LGFae/swww ~/Work/swww
+cd ~/Work/swww || exit 2
 cargo build --release
 sudo cp target/release/swww-daemon /usr/local/bin/
 sudo cp target/release/swww /usr/local/bin/
 
 # PYWAL
-cd ~/Work
-git clone https://github.com/dylanaraps/pywal
-cd pywal
+git clone https://github.com/dylanaraps/pywal ~/Work/pywal
+cd ~/Work/pywal || exit 2
 ~/.venv/bin/pip3 install pywal
 
 # LIBDRM > 2.4.119
-cd ~/Work
-git clone https://gitlab.freedesktop.org/mesa/drm.git
-cd drm
+git clone https://gitlab.freedesktop.org/mesa/drm.git ~/Work/drm
+cd ~/Work/drm || exit 2
 meson setup --reconfigure --prefix=/usr/local build
 ninja -C build
-ninja -C build install  
-
+ninja -C build install
 
 # HYPRLAND
-cd ~/Work
-git clone https://github.com/hyprwm/Hyprland --recurse-submodules
-cd Hyprland
+git clone https://github.com/hyprwm/Hyprland --recurse-submodules ~/Work/hyprland/
+cd ~/Work/Hyprland || exit 2
 meson setup --reconfigure --prefix=/usr/local build
 ninja -C build
 ninja -C build install
 
 # SWAY IDLE
-cd ~/Work
-git clone https://github.com/swaywm/swayidle
-cd swayidle
+git clone https://github.com/swaywm/swayidle ~/Work/swayidle
+cd ~/Work/swayidle || exit 2
 meson setup --reconfigure --prefix=/usr/local build
 ninja -C build
 ninja -C build install
 
 # WLOGOUT
-cd ~/Work
-git clone https://github.com/swaywm/wlogout
-cd wlogout
+git clone https://github.com/swaywm/wlogout ~/Work/wlogout
+cd ~/Work/wlogout || exit 2
 meson setup --reconfigure --prefix=/usr/local build
 ninja -C build
 ninja -C build install
 
 # WAYBAR
-sudo apt install clang-tidy gobject-introspection libdbusmenu-gtk3-dev libevdev-dev libfmt-dev libgirepository1.0-dev libgtk-3-dev libgtkmm-3.0-dev libinput-dev libjsoncpp-dev libmpdclient-dev libnl-3-dev libnl-genl-3-dev libpulse-dev libsigc++-2.0-dev libspdlog-dev libwayland-dev scdoc upower libxkbregistry-dev
-
-cd ~/Work
-git clone https://github.com/Alexays/Waybar
-cd Waybar
+git clone https://github.com/Alexays/Waybar ~/Work/Waybar
+cd ~/Work/Waybar || exit 2
 meson setup --reconfigure --prefix=/usr/local build
 ninja -C build
 ninja -C build install
 
+# XDG DESKTOP PORTAL HYPRLAND
+
+git clone --recursive https://github.com/hyprwm/xdg-desktop-portal-hyprland ~/Work/xdg-desktop-portal-hyprland
+cd ~/Work/xdg-desktop-portal-hyprland/ || exit 2
+cmake -DCMAKE_INSTALL_LIBEXECDIR=/usr/local/lib -DCMAKE_INSTALL_PREFIX=/usr/local -B build
+cmake --build build
+sudo cmake --install build
+
 # YOUTUBE MUSIC
-cd ~Work
-git clone https://github.com/th-ch/youtube-music
-cd youtube-music
+git clone https://github.com/th-ch/youtube-music ~/Work/youtube-music
+cd ~/Work/youtube-music || exit 2
 npm i --legacy-peer-deps
 sudo cp dist/*.AppImage /usr/local/bin/youtube-music
